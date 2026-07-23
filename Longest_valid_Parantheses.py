938870afe09035ee73a97360134bc68968a02be4
Pari-Agarwal03 <agarwalpari329@gmail.com>
2026-07-23 13:06:38 +0530

    Add files via upload

---
+++ Longest_valid_Parantheses.py
@@ -0,0 +1,15 @@
+def longestValidParentheses(s):
+    stack = [-1]          # help to get start point 
+    max_length = 0
+    for i in range(len(s)):
+        if s[i] == '(':
+            stack.append(i)
+        else:
+            stack.pop()
+
+            if len(stack) == 0:
+                stack.append(i)
+            else:
+                max_length = max(max_length, i - stack[-1])
+
+    return max_length
